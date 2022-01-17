35de7044406af87706d8feec0ab32077950990c7
Adrien Parrochia <adrien.parrochia@adevinta.com>
2022-01-17 13:11:55 +0100

    update oauth

--- server/main.go
+++ server/main.go
@@ -12,7 +12,6 @@ import (
 	"github.com/go-oauth2/oauth2/v4/store"
 	"github.com/go-session/session"
 	"github.com/google/uuid"
-	guuid "github.com/google/uuid"
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 	"html/template"
@@ -22,16 +21,60 @@ import (
 	"time"
 )
 
+/*
 var (
 	clientID     string = uuid.New().String()[:8]
 	clientSecret string = uuid.New().String()[:8]
-)
+)*/
 
 const (
-	sessionLabelUserID string = "LoggedInUserID"
-	sessionLabelAccessToken string = "LoggedAccessToken"
+	sessionLabelUserID       string = "LoggedInUserID"
+	sessionLabelAccessToken  string = "LoggedAccessToken"
 	sessionLabelRefreshToken string = "LoggedRefreshToken"
 )
+
+type Address string
+
+func (a Address) String() string {
+	return string(a)
+}
+
+type userData struct {
+	clientID     string
+	clientSecret string
+	address      Address
+	name         string
+	email        string
+	password     string
+}
+
+var database = map[Address]userData{
+	"address 1": {
+		clientID:     uuid.New().String()[:8],
+		clientSecret: uuid.New().String()[:8],
+		address:      "address 1",
+		name:         "adrien",
+		email:        "adrienparrochia@gmail.com",
+		password:     "toto",
+	},
+	"address 2": {
+		clientID:     uuid.New().String()[:8],
+		clientSecret: uuid.New().String()[:8],
+		address:      "address 2",
+		name:         "adrien+2",
+		email:        "adrienparrochia+2@gmail.com",
+		password:     "toto",
+	},
+	"address 3": {
+		clientID:     uuid.New().String()[:8],
+		clientSecret: uuid.New().String()[:8],
+		address:      "address 3",
+		name:         "adrien+3",
+		email:        "adrienparrochia+3@gmail.com",
+		password:     "toto",
+	},
+}
+
 // https://github.com/go-oauth2/oauth2/blob/b208c14e621016995debae2fa7dc20c8f0e4f6f8/example/server/server.go
 // https://github.com/go-oauth2/oauth2/blob/fa969a085ba42725f9b957c744ec5ba6d548b4ab/manage/manage_test.go
 func main() {
@@ -49,13 +92,13 @@ func main() {
 	// manager.MapAccessGenerate(generates.NewJWTAccessGenerate("", []byte("00000000"), jwt.SigningMethodHS512))
 	manager.MapAccessGenerate(generates.NewAccessGenerate())
 
+	// you can (un)comment this to set a user specific client ID
 	clientStore := store.NewClientStore()
-	clientStore.Set(clientID, &models.Client{
+	/*clientStore.Set(clientID, &models.Client{
 		ID:     clientID,
 		Secret: clientSecret,
 		// Domain: "http://127.0.0.1:9096",
-	})
-
+	})*/
 	manager.MapClientStorage(clientStore)
 
 	srv := server.NewServer(server.NewConfig(), manager)
@@ -85,21 +128,21 @@ func main() {
 			return
 		}
 
-		userID = uid.(string)
+		userID = uid.(Address).String()
 		store.Delete(sessionLabelUserID)
 		store.Save()
 		return
 	})
 
-/*
-	srv.SetPasswordAuthorizationHandler(func(username, password string) (userID string, err error) {
-		fmt.Println("************************************** SetPasswordAuthorizationHandler")
-		if username == "test" && password == "test" {
-			userID = "test"
-		}
-		return
-	})
-*/
+	/*
+		srv.SetPasswordAuthorizationHandler(func(username, password string) (userID string, err error) {
+			fmt.Println("************************************** SetPasswordAuthorizationHandler")
+			if username == "test" && password == "test" {
+				userID = "test"
+			}
+			return
+		})
+	*/
 
 	srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
 		log.Println("Internal Error:", err.Error())
@@ -111,66 +154,72 @@ func main() {
 
 	r := mux.NewRouter()
 	// login page
-	r.HandleFunc("/login", loginHandler)
+	r.HandleFunc("/login", hasValidToken(func(w http.ResponseWriter, r *http.Request) {
+		outputHTML(w, "login", struct{}{})
+	}, srv))
 	// login page call credentials
 	r.HandleFunc("/credentials", func(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
 
-		username := r.FormValue("username")
+		email := r.FormValue("email")
 		password := r.FormValue("password")
 
-		fmt.Println(fmt.Sprintf("%s : %s", username, password))
-		// call user verification here
+		userData := getUserDataWithLogin(email, password)
+		if userData == nil {
+			http.Error(w, "user not found", http.StatusNotFound)
+			return
+		}
 
-		userID := guuid.New().String()
+		// call user verification here
 		store, err := session.Start(r.Context(), w, r)
 		if err != nil {
 			return
 		}
-		store.Set(sessionLabelUserID, userID)
+		store.Set(sessionLabelUserID, userData.address)
 		store.Save()
 
-		/*
-			// your pubkey
-			clientID := uuid.New().String()[:8]
-			// your privateKey
-			clientSecret := uuid.New().String()[:8]
-
-			if err := clientStore.Set(clientID, &models.Client{
-				ID:     clientID,
-				Secret: clientSecret,
-				Domain: "http://localhost:9094",
-				// UserID: address,
-			}); err != nil {
-				fmt.Println(err.Error())
-			}
-		*/
-
+		if err := clientStore.Set(userData.clientID, &models.Client{
+			ID:     userData.clientID,
+			Secret: userData.clientSecret,
+			// Domain: "http://localhost:9094",
+		}); err != nil {
+			fmt.Println(err.Error())
+		}
 		/* call token or authorize endpoint/ */
-		/*
-		w.Header().Set("Location", "/token?grant_type=client_credentials"+
-			"&client_id="+clientID+
-			"&client_secret="+clientSecret+
-			"&scope=all")
-		*/
-
-		/*w.Header().Set("Location", "/token?grant_type=refresh_token"+
-			"&client_id="+clientID+
-			"&client_secret="+clientSecret+
-			"&refresh_token="+
-			"&scope=all")*/
-
-		w.Header().Set("Location", "/authorize?grant_type=client_credentials"+
-			"&client_id="+clientID+
-			"&client_secret="+clientSecret+
-			"&scope=all"+
-			"&response_type=token")
-
+		/*w.Header().Set("Location", "/token")*/
+		w.Header().Set("Location", "/authorize")
 		w.WriteHeader(http.StatusFound)
 	})
 
 	r.HandleFunc("/authorize", func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
+
+		parm := r.Form
+		if parm == nil {
+			parm = url.Values{}
+		}
+
+		// user authorization
+		address, err := srv.UserAuthorizationHandler(w, r)
+		if err != nil {
+			//return s.redirectError(w, req, err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			// err := srv.redirectError(w, req, err)}
+			return
+		} else if address == "" {
+			w.Header().Set("Location", "/login")
+			return
+		}
+
+		userData := getUserDataWithAddress(address)
+
+		parm.Add("grant_type", "client_credentials")
+		parm.Add("client_id", userData.clientID)
+		parm.Add("client_secret", userData.clientSecret)
+		parm.Add("scope", "all")
+		parm.Add("response_type", "token")
+
+		r.Form = parm
 
 		req, err := srv.ValidationAuthorizeRequest(r)
 		if err != nil {
@@ -178,18 +227,7 @@ func main() {
 			// err := srv.redirectError(w, req, err)}
 			return
 		}
-
-		// user authorization
-		userID, err := srv.UserAuthorizationHandler(w, r)
-		if err != nil {
-			//return s.redirectError(w, req, err)
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			// err := srv.redirectError(w, req, err)}
-			return
-		} else if userID == "" {
-			return
-		}
-		req.UserID = userID
+		req.UserID = address
 
 		// specify the scope of authorization
 		if fn := srv.AuthorizeScopeHandler; fn != nil {
@@ -244,50 +282,11 @@ func main() {
 	})
 
 	r.HandleFunc("/token", func(w http.ResponseWriter, r *http.Request) {
-		store, err := session.Start(r.Context(), w, r)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		refreshToken, ok := store.Get(sessionLabelRefreshToken)
-		if !ok {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		parm := r.Form
-		if parm == nil {
-			parm = url.Values{}
-		}
-
-		parm.Add("refresh_token", refreshToken.(string))
-		parm.Add("grant_type", oauth2.Refreshing.String())
-		parm.Add("client_id", clientID)
-		parm.Add("client_secret", clientSecret)
-		parm.Add("scope", "all")
-
-		r.Form = parm
-
-		ctx := r.Context()
-
-		gt, tgr, err := srv.ValidationTokenRequest(r)
-		if err != nil {
+		if err := refreshToken(srv, w, r); err != nil {
 			_, statusCode, _ := srv.GetErrorData(err)
-			http.Error(w, "fail to valid token request", statusCode)
+			http.Error(w, err.Error(), statusCode)
 			return
 		}
-
-		ti, err := srv.GetAccessToken(ctx, gt, tgr)
-		if err != nil {
-			_, statusCode, _ := srv.GetErrorData(err)
-			http.Error(w, "fail to valid token request", statusCode)
-			return
-		}
-
-		data := srv.GetTokenData(ti)
-		store.Set(sessionLabelAccessToken, data["access_token"].(string))
-		store.Set(sessionLabelRefreshToken, data["refresh_token"].(string))
-		store.Save()
-
 		w.Header().Set("Location", "/protected")
 		w.WriteHeader(http.StatusFound)
 	})
@@ -315,7 +314,7 @@ func main() {
 
 		linkRefreshToken := "/token"
 
-		outputHTML(w,"protected", struct {
+		outputHTML(w, "protected", struct {
 			User string
 			Link string
 		}{
@@ -334,10 +333,6 @@ func main() {
 	log.Fatal(http.ListenAndServe(":9096", corsWrapper.Handler(r)))
 }
 
-func loginHandler(w http.ResponseWriter, r *http.Request) {
-	outputHTML(w,"login", struct {}{})
-}
-
 func outputJSON(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
 	w.Header().Set("cache-control", "no-cache,no-store")
@@ -347,27 +342,56 @@ func outputJSON(w http.ResponseWriter, data interface{}) {
 }
 
 func outputHTML(w http.ResponseWriter, filename string, data interface{}) {
-	t := template.Must(template.ParseFiles("static/"+filename+".tmpl"))
+	t := template.Must(template.ParseFiles("static/" + filename + ".tmpl"))
 	err := t.Execute(w, data)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func validateToken(f http.HandlerFunc, srv *server.Server) http.HandlerFunc {
+func hasValidToken(f http.HandlerFunc, srv *server.Server) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		store, err := session.Start(r.Context(), w, r)
 		if err != nil {
 			return
 		}
 		accessToken, ok := store.Get(sessionLabelAccessToken)
+		// si pas de token, on reste sur la page
 		if !ok {
-			http.Error(w, "fail to get access token", http.StatusBadRequest)
+			f.ServeHTTP(w, r)
+			return
+		}
+		// si token valide, on change de page
+		if _, err := srv.Manager.LoadAccessToken(r.Context(), accessToken.(string)); err == nil {
+			w.Header().Set("Location", "/protected")
+			w.WriteHeader(http.StatusFound)
+			return
+		}
+		// on reste sur la page
+		f.ServeHTTP(w, r)
+	})
+}
+
+func validateToken(f http.HandlerFunc, srv *server.Server) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		store, err := session.Start(r.Context(), w, r)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		accessToken, ok := store.Get(sessionLabelAccessToken)
+		if !ok {
+			w.Header().Set("Location", "/login")
+			w.WriteHeader(http.StatusFound)
 			return
 		}
 		if _, err := srv.Manager.LoadAccessToken(r.Context(), accessToken.(string)); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
+			// try to refresh token
+			if err = refreshToken(srv, w, r); err != nil {
+				w.Header().Set("Location", "/login")
+				w.WriteHeader(http.StatusFound)
+				return
+			}
 		}
 		/*data := map[string]interface{}{
 			"expires_in": int64(token.GetAccessCreateAt().Add(token.GetAccessExpiresIn()).Sub(time.Now()).Seconds()),
@@ -376,4 +400,71 @@ func validateToken(f http.HandlerFunc, srv *server.Server) http.HandlerFunc {
 		}*/
 		f.ServeHTTP(w, r)
 	})
+}
+
+func refreshToken(srv *server.Server, w http.ResponseWriter, r *http.Request) error {
+	store, err := session.Start(r.Context(), w, r)
+	if err != nil {
+		return err
+	}
+	refreshToken, ok := store.Get(sessionLabelRefreshToken)
+	if !ok {
+		return err
+	}
+
+	rti, err := srv.Manager.LoadRefreshToken(r.Context(), refreshToken.(string))
+	if err != nil {
+		return err
+	}
+	userData := getUserDataWithAddress(rti.GetUserID())
+
+	parm := r.Form
+	if parm == nil {
+		parm = url.Values{}
+	}
+	parm.Add("refresh_token", refreshToken.(string))
+	parm.Add("grant_type", oauth2.Refreshing.String())
+	parm.Add("client_id", userData.clientID)
+	parm.Add("client_secret", userData.clientSecret)
+	parm.Add("scope", "all")
+
+	r.Form = parm
+
+	ctx := r.Context()
+
+	gt, tgr, err := srv.ValidationTokenRequest(r)
+	if err != nil {
+		return err
+	}
+
+	ti, err := srv.GetAccessToken(ctx, gt, tgr)
+	if err != nil {
+		return err
+	}
+
+	data := srv.GetTokenData(ti)
+	store.Set(sessionLabelAccessToken, data["access_token"].(string))
+	store.Set(sessionLabelRefreshToken, data["refresh_token"].(string))
+	store.Save()
+	return nil
+}
+
+// func for simulate database
+
+func getUserDataWithLogin(email, password string) *userData {
+	for _, s := range database {
+		if s.email == email && s.password == password {
+			return &s
+		}
+	}
+	return nil
+}
+
+func getUserDataWithAddress(ad string) *userData {
+	for a, data := range database {
+		if a == Address(ad) {
+			return &data
+		}
+	}
+	return nil
 }
